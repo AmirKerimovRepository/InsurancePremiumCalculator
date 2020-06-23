2a1c400957835fa17721b9a40cf5f2655939c4e0
AmirKerimovRepository <amirkerimov915@gmail.com>
2020-06-23 22:20:35 +0300

    message

--- src/test/java/com/example/InsurancePremiumCalculator/validation/RiskTypeFireTest.java
+++ src/test/java/com/example/InsurancePremiumCalculator/validation/RiskTypeFireTest.java
@@ -10,11 +10,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RiskTypeFireTest {
 
+    //set_coefficient need to be 0.014 , actual = 10 * 0.014 = 0.14
     @Test
-    public void shouldCalculatePremiumFire() {
+    public void shouldCalculatePremiumFireWithDefaultValue() {
         RiskTypeFire riskTypeFire = new RiskTypeFire(BigDecimal.valueOf(10));
         BigDecimal actual = riskTypeFire.apply();
         assertEquals(BigDecimal.valueOf(0.14), actual);
+    }
+    //set_coefficient need to be 0.024 , actual  = 120 * 0.024 = 2.88
+    @Test
+    public void shouldCalculatePremiumFireWithBiggerValue() {
+        RiskTypeFire riskTypeFire = new RiskTypeFire(BigDecimal.valueOf(120));
+        BigDecimal actual = riskTypeFire.apply();
+        assertEquals(BigDecimal.valueOf(2.88), actual);
     }
 
     @Test
--- src/test/java/com/example/InsurancePremiumCalculator/validation/RiskTypeTheftTest.java
+++ src/test/java/com/example/InsurancePremiumCalculator/validation/RiskTypeTheftTest.java
@@ -10,11 +10,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RiskTypeTheftTest {
 
+    //set_coefficient need to be 0.05 , actual = 10 * 0.05 = 0.5
     @Test
-    public void shouldCalculatePremiumFire() {
+    public void shouldCalculatePremiumTheftWithDefaultValue() {
         RiskTypeTheft riskTypeTheft = new RiskTypeTheft(BigDecimal.valueOf(10));
         BigDecimal actual = riskTypeTheft.apply();
-        assertEquals(BigDecimal.valueOf(0.14), actual);
+        assertEquals(BigDecimal.valueOf(0.5), actual);
+    }
+    //set_coefficient need to be 0.11 , actual = 20 * 0.11 = 2.2
+    @Test
+    public void shouldCalculatePremiumFireWithBiggerValue() {
+        RiskTypeFire riskTypeFire = new RiskTypeFire(BigDecimal.valueOf(20));
+        BigDecimal actual = riskTypeFire.apply();
+        assertEquals(BigDecimal.valueOf(2.2), actual);
     }
 
     @Test
